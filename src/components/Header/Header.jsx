8cf34ac2be84fc3be573302e31d0c391a16d830e
Erik Hernandez <erikestuardo_161@hotmail.com>
2022-11-22 00:00:48 -0600

    Header Component JSX

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+import '../Nav/Nav';
+
+const Header = () => {
+  return (
+    <div className="l-header">
+        <Nav/>
+    </div>
+  )
+}
+
+export default Header
